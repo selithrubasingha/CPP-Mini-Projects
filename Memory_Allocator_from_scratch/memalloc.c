9bc78f8053227a5bd0d629c6fb663caa25f496ab
Selith <selithrubasingha@gmail.com>
2025-12-28 16:51:54 +0530

    build advanced malloc and get free block logic

--- Memory_Allocator_from_scratch/memalloc.c
+++ Memory_Allocator_from_scratch/memalloc.c
@@ -1,7 +1,10 @@
 #include <unistd.h>  // Required for sbrk
 #include <stddef.h>  // Required for size_t (or use <stdlib.h>)
-
+#include <pthread.h> // For pthread_mutex_lock, pthread_mutex_t
 typedef char ALIGN[16];
+
+pthread_mutex_t global_malloc_lock;
+header_t *head, *tail;
 
 struct header_t {
     size_t size;
@@ -9,6 +12,7 @@ struct header_t {
     struct header_t* next;
 };
 
+//union is like a struct but it can  force a specific size ... in this case 16 bytes
 union header {
     struct  {
     size_t size;
@@ -16,6 +20,7 @@ union header {
     struct header_t* next;
     }s;
 
+    //the structure stretcher
     ALIGN stub;
 };
 
@@ -23,13 +28,53 @@ typedef union header header_t;
 
 
 void *malloc(size_t size){
-    void* block;
 
+    size_t total_size ;
+    void* block;
+    header_t* header;
+
+    if (!size) return NULL;
+
+    pthread_mutex_lock(&global_malloc_lock);
+    header = get_free_block(size);
+
+    if (header){
+        header->s.is_free = 0 ;
+        pthread_mutex_unlock(&global_malloc_lock);
+        return (void*)(header+1);
+    }
+
+    total_size = sizeof(header_t)+size;
+    
     //sbrk-> set pointer break ... increase heap size by "size" amount 
     block = sbrk(size);
 
     //void* is for type casting -1 into a void pointer
-    if (block== (void*)-1) return NULL;
+    if (block== (void*)-1){ 
+        pthread_mutex_lock(&global_malloc_lock);
+        return NULL;
+    }
+    header = block;
+    header->s.size = size;
+    header->s.is_free = 0 ;
+    header->s.next = NULL;
 
-    return block ;
+    if (!head) head = header;
+
+    if (tail) tail->s.next = header;
+
+    tail = header;
+
+	pthread_mutex_unlock(&global_malloc_lock);
+	return (void*)(header + 1);
+}
+
+header_t* get_free_block(size_t size){
+    header_t* curr = head ; 
+    while (curr) {
+        if (curr->s.is_free && curr->s.size >=size)
+            return curr;
+        curr = curr->s.next;
+    }
+    return NULL;
 }
